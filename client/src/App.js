33b36fc248e5e4f53f656abf372ce96a3542d025
Josh McCormick <josh.mccormick81@gmail.com>
2018-12-03 13:59:09 -0500

    added basics

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import * as $ from 'axios';
+
+
+
+class App extends React.Component {
+
+  state = {
+    notesList: [],
+    newNote: '',
+    isUpdating:false
+  }
+
+  
+
+ 
+}
+
+export default App;
