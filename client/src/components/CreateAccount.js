3c3e349225bc45312c3531c50c0f926894cc1855
Josh McCormick <josh.mccormick81@gmail.com>
2018-12-04 17:57:47 -0500

    created a rough, and not tested Form component to create an account for our website. The functions should all work, but the form is very basic and not pretty at all. It's just for skeleton purposes and we can pretty it up tomorrow.

---
+++ client/src/components/CreateAccount.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import * as $ from "axios";
+import AppBar from "./components/AppBar";
+
+const Form=(props)=>(
+  <form>
+    <input type="text" name="username" value={props.userVal} onChange={props.handleChange} />
+    <input type="text" name="password" value={props.passVal} onChange={props.handleChange} />
+    <input type="text" name="nickname" value={props.nickVal} onChange={props.handleChange} />
+    <input type="text" name="airport" value={props.airVal} onChange={props.handleChange} />
+    <input type="text" name="email" value={props.eVal} onChange={props.handleChange} />
+    <input type="text" name="budget" value={props.budgetVal} onChange={props.handleChange} />
+    <button type="submit" onClick={props.submitUser}>Create Account</button>
+  </form>
+)
+
+class AccountCreate extends React.Component {
+  state = {
+    username:'',
+    password: '',
+    nickname:'',
+    airport:'',
+    email:'',
+    budget:''
+  };
+
+  componentDidMount() {
+  }
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]:e.target.value
+    })
+  }
+  createUser=(e)=>{
+    e.preventDefault();
+    const userData={
+      username=this.state.username,
+      password=this.state.password,
+      nicname=this.state.nickname,
+      airport=this.state.airport,
+      email=this.state.email,
+      budget=this.state.budget
+    }
+    $.post({
+      url:'/api/users',
+      data:userData
+    })
+    .then(function(resp){
+      alert('Thanks for Creating an account, Please Login to continue.')
+    })
+    .catch(err=>{
+      alert('An Error Occured. Please make sure to fill out all fields.')
+    })
+  }
+  render() {
+    return <Form onChange={this.handleChange} userVal={this.state.username} passVal={this.state.password} nickVal={this.state.nickname} airVal={this.state.airport} budgetVal={this.state.budget} eVal={this.state.email} onClick={this.createUser}/>;
+  }
+}
+
+export default AccountCreate;
