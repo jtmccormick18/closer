dce191bdbda3fa79d16427504aff98ed00ea045f
Josh McCormick <josh.mccormick81@gmail.com>
2018-12-04 14:14:45 -0500

    added API route for retrieving a User, and its associated partners and Trips

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,26 @@
-const db=require('../models')
-module.exports=function(app){
-    app.get('/api/users',function(req,res){
-        db.User.findAll({}).then(function(users){
+const db = require('../models')
+module.exports = function (app) {
+    app.get('/api/users', function (req, res) {
+        db.User.findAll({}).then(function (users) {
             res.json(users);
+        })
+    })
+    app.get('/api/users/:id', function (req, res) {
+        db.User.find({
+            where: {
+                id:req.params.id
+            },
+            include:[
+                {
+                model:db.Partner
+            },
+            {
+                model:db.Trip
+            }]
+        }).then(resp=>{
+            res.json(resp)
+        }).catch(err=>{
+            res.json(err);
         })
     })
 }
