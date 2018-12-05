bf97f8e790c50e8aa6d87339e3d15303652fc6f5
unknown <bhandari.neel@outlook.com>
2018-12-05 11:55:10 -0500

    Added .env file to secure API keys

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express= require ('express');
 const app=express();
 const path=require('path');
 const PORT=process.env.PORT||8080
+require('dotenv').config();
 
 
 app.use(express.urlencoded({ extended: true }));
