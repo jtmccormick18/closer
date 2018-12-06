a0575e97e3fc8ad16f6fb21dd6dac485a5ce5142
Josh McCormick <josh.mccormick81@gmail.com>
2018-12-06 14:01:28 -0500

    added account

---
+++ client/src/components/CreateAccount.js
@@ -0,0 +1,157 @@
+import React from "react";
+import * as $ from "axios";
+import MenuItem from '@material-ui/core/MenuItem';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Airport from "./Airport";
+
+const Form = props => (
+  <Grid container justify="center">
+    <form className="createAccountForm" >
+      <Typography align="center" variant="h4" color="inherit">
+        Create an Account
+          </Typography>
+      <hr />
+      <MenuItem>
+        <span className="menuLabel">Username:</span>
+    <input
+          className="menuitem"
+          type="text"
+          name="username"
+          value={props.userVal}
+          onChange={props.handleChange}
+
+        />
+      </MenuItem>
+      <MenuItem>
+      <span className="menuLabel">Password:</span>
+    <input
+          className="menuitem"
+          type="password"
+          name="password"
+          value={props.passVal}
+          onChange={props.handleChange}
+        /> </MenuItem>
+      <MenuItem>
+      <span className="menuLabel">Nickname:</span>
+    <input
+<<<<<<< HEAD
+          type="text"
+          name="nickname"
+          value={props.nickVal}
+          onChange={props.handleChange}
+        /></MenuItem> <MenuItem>
+        Airport:
+    <input
+          type="text"
+          name="airport"
+          value={props.airVal}
+          onChange={props.handleChange}
+        /></MenuItem>
+      <MenuItem>
+        Email:
+=======
+      className="menuitem"
+      type="text"
+      name="nickname"
+      value={props.nickVal}
+      onChange={props.handleChange}
+    /></MenuItem> 
+  
+    <MenuItem>
+    <span className="menuLabel">Email</span>
+>>>>>>> 59c9953bd068389e8ac571230e11ee9f6cfd7fdb
+    <input
+          className="menuitem"
+          type="text"
+          name="email"
+          value={props.eVal}
+          onChange={props.handleChange}
+        /></MenuItem>
+      <MenuItem>
+      <span className="menuLabel">Budget:</span>
+    <input
+<<<<<<< HEAD
+=======
+      className="menuitem"
+>>>>>>> 59c9953bd068389e8ac571230e11ee9f6cfd7fdb
+      type="text"
+      name="budget"
+      value={props.budgetVal}
+      onChange={props.handleChange}
+    /></MenuItem>
+    <Airport onChange={props.handleChange} currAirport={props.airVal} />
+    <div className="buttonbox">
+    <Button align="center" type="submit" onClick={props.submitUser}>
+      Create Account
+    </Button>
+    </div>
+    </form>
+  </Grid>
+);
+
+class AccountCreate extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    nickname: "",
+    airport: "",
+    email: "",
+    budget: ""
+  };
+
+  componentDidMount() {}
+<<<<<<< HEAD
+  handleChange = (e) => {
+=======
+  handleChange = (e, values) => {
+    values = e.target.name ? e.target : values;
+    const {name, value} = values;
+>>>>>>> 59c9953bd068389e8ac571230e11ee9f6cfd7fdb
+    this.setState({
+      [name]: value
+    });
+  };
+
+  createUser = e => {
+    e.preventDefault();
+    const userData = {
+      username: this.state.username,
+      password: this.state.password,
+      nickname: this.state.nickname,
+      airport: this.state.airport,
+      email: this.state.email,
+      budget: this.state.budget
+    };
+    console.log(userData)
+    $.post("/api/users", userData)
+      .then(resp => {
+        console.log(resp);
+        alert("Thanks for Creating an account, Please Login to continue.");
+      })
+      .catch(err => {
+        alert('Fill out the entire form!')
+      })
+  };
+
+  render() {
+    return (
+      <div>
+        
+        <Form
+          handleChange={this.handleChange}
+          userVal={this.state.username}
+          passVal={this.state.password}
+          nickVal={this.state.nickname}
+          airVal={this.state.airport}
+          budgetVal={this.state.budget}
+          eVal={this.state.email}
+          submitUser={this.createUser}
+        />
+      </div>
+    );
+  }
+}
+
+export default AccountCreate;
