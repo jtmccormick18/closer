526992247968f0c3757451d4746d4b96b45b8498
unknown <bhandari.neel@outlook.com>
2018-12-10 14:19:05 -0500

    Protected the routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -137,4 +137,9 @@ module.exports = function (app) {
             res.json(err);
         })
     });
+
+    //creating a Authentication route
+    app.get('/check', authentication, function(req,res){
+        res.status(200).end();
+    })
 }
