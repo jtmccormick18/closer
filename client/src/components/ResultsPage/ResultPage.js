cb2e37b032c477a69594048815e13a22ac17c689
Sukjune Hong <sukjune.hong@gmail.com>
2018-12-12 12:35:13 -0500

    Fixes a whole lot of pesky bugs

--- client/src/components/ResultsPage/ResultPage.js
+++ client/src/components/ResultsPage/ResultPage.js
@@ -7,6 +7,8 @@ import * as $ from "axios";
 import YourPartner from "./YourPartner";
 import Grid from "@material-ui/core/Grid";
 import { Typography } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent"
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
@@ -20,6 +22,12 @@ const styles = theme => ({
     padding: theme.spacing.unit * 2,
     textAlign: "center",
     color: theme.palette.text.secondary
+  },
+  card: {
+    minWidth: 200,
+  },
+  pos: {
+    marginBottom: 12,
   }
 });
 
@@ -45,15 +53,53 @@ HotelDisplay.propTypes = {
 };
 HotelDisplay = withStyles(styles)(HotelDisplay);
 
-const Flights = props => (
-  <div>
-    <h1>
-      {props.embark} to {props.dest}
-    </h1>
-    <p>Airline: {props.airline}</p>
-    <p>Flight Number: {props.flightNo}</p>
-  </div>
-);
+function FlightCard(props) {
+  const { classes } = props;
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography variant="headline" component="h2">
+          {props.embark} to {props.dest}
+        </Typography>
+        <Typography variant="h6" component="h2">
+          {props.airline}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Airline
+        </Typography>
+        <Typography variant="h6" component="h2">
+          {props.flightNo}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Flight Number
+        </Typography>
+        <Typography variant="h6" component="h2">
+          ${props.totalFare}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Total Cost
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+FlightCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+FlightCard = withStyles(styles)(FlightCard);
+
+// const Flights = props => (
+//   <div>
+//     <h1>
+//       {props.embark} to {props.dest}
+//     </h1>
+//     <p>Airline: {props.airline}</p>
+//     <p>Flight Number: {props.flightNo}</p>
+//     <p>Total Cost: ${props.totalFare}</p>
+//   </div>
+// );
 
 class ResultPage extends React.Component {
   state = {
@@ -73,6 +119,8 @@ class ResultPage extends React.Component {
     hasFlights: false,
     partnerAir: {},
     userAir: {},
+    userTotalFare: null,
+    partnerTotalFare: null,
     return: "",
     s22obj: {
       aid: 'clsrapp', // your affiliate id for tracking
@@ -89,6 +137,7 @@ class ResultPage extends React.Component {
     },
     params22: ""
   };
+
   setParams22 = () => {
     let date1 = this.state.departure.split('-');
     let date2 = this.state.return.split('-');
@@ -96,8 +145,8 @@ class ResultPage extends React.Component {
       aid: 'clsrapp',
       checkin: `${date1[0]}${date1[1]}${date1[2]}`,
       checkout: `${date2[0]}${date2[1]}${date2[2]}`,
-      lat: this.state.midpoint[0],
-      lng: this.state.midpoint[1],
+      lat: this.state.midpoint[1],
+      lng: this.state.midpoint[0],
       maincolor: '00549E',
       markerimage: "https://www.stay22.com/logo.png"
     };
@@ -120,8 +169,8 @@ class ResultPage extends React.Component {
     let date1 = this.state.departure.split("-");
     let date2 = this.state.return.split("-");
     let data = {
-      lat: this.state.midpoint[0],
-      long: this.state.midpoint[1]
+      lat: this.state.midpoint[1],
+      long: this.state.midpoint[0]
     }
 
     $.post('/api/midpoint', data)
@@ -133,47 +182,58 @@ class ResultPage extends React.Component {
           destCity: midpointRes.data[0][0].airport_name
         })
 
+        if (destITACode === this.state.partner_airport || destITACode === this.state.airport)  {
+          this.setParams22()
+        } else {
+
         $.get(`https://developer.goibibo.com/api/search/?app_id=ad6a1a69&app_key=dcf3fe52cb4920b668f623315303b99f&format=json&source=${this.state.airport}&destination=${destITACode}&dateofdeparture=${date1[0]}${date1[1]}${date1[2]}&seatingclass=E&adults=1&children=0&infants=0&counter=100`)
           .then(userFlights => {
-            console.log({ userFlights })
+            const { onwardflights } = userFlights.data.data;
+
             let directFlights;
-            for (let i = 0; i < userFlights.data.data.onwardflights.length; i++) {
-              if (userFlights.data.data.onwardflights[i].destination === destITACode) {
-                directFlights = userFlights.data.data.onwardflights[i]
+            for (let i = 0; i < onwardflights.length; i++) {
+              if (onwardflights[i].destination === destITACode) {
+                directFlights = onwardflights[i]
               }
             }
-            console.log(directFlights)
+            const fareParsed = directFlights.fare.totalfare / 100;
             this.setState({
-              userAir: directFlights
+              userAir: directFlights,
+              userTotalFare: fareParsed
             })
           })
 
         $.get(`https://developer.goibibo.com/api/search/?app_id=ad6a1a69&app_key=dcf3fe52cb4920b668f623315303b99f&format=json&source=${this.state.partner_airport}&destination=${destITACode}&dateofdeparture=${date1[0]}${date1[1]}${date1[2]}&seatingclass=E&adults=1&children=0&infants=0&counter=100`)
           .then(res => {
             let directFlights;
+            console.log(res);
+            debugger;
             for (let i = 0; i < res.data.data.onwardflights.length; i++) {
               if (res.data.data.onwardflights[i].destination === destITACode) {
                 directFlights = res.data.data.onwardflights[i]
               }
             }
+            const fareParsed = directFlights.fare.totalfare / 100;
             this.setState({
               partnerAir: directFlights,
-              hasFlights: true
+              hasFlights: true,
+              partnerTotalFare: fareParsed
             })
           })
-      });
-    this.setParams22();
-  }
+          this.setParams22();
+      };
+  })}
 
   getMidPoint = () => {
     $.get(`/airports/${localStorage.clsr_id}`)
       .then(resp => {
+
         let userLat = parseFloat(resp.data[0][0].LAT1);
         let userLong = parseFloat(resp.data[0][0].LONG1);
         let partLat = parseFloat(resp.data[0][0].LAT2);
         let partLong = parseFloat(resp.data[0][0].LONG2);
-        let point1 = turf.point([userLat, userLong]);
-        let point2 = turf.point([partLat, partLong]);
+        let point1 = turf.point([userLong, userLat]);
+        let point2 = turf.point([partLong, partLat]);
         let midpoint = turf.midpoint(point1, point2);
         let date1 = this.state.departure.split('-');
         let date2 = this.state.return.split('-');
@@ -185,6 +245,7 @@ class ResultPage extends React.Component {
           airportB: [partLat, partLong],
           midpoint: midpoint.geometry.coordinates
         })
+        console.log(this.state);
       })
   }
 
@@ -197,10 +258,12 @@ class ResultPage extends React.Component {
           airport2: resp.data.Partner.partner_airport
         })
       })
-  }
+  };
+
   componentDidMount() {
     this.getMidPoint();
   }
+  
   getUserInfo = () => {
     $.get(`/api/users/${localStorage.clsr_id}`).then(resp => {
       this.setState({
@@ -232,58 +295,72 @@ class ResultPage extends React.Component {
             handleSubmit={this.getFlights}
           />
         ) : (
-          <Grid container spacing={12}>
-            <Grid item xs={1}>
-              <div />
-            </Grid>
-            <Grid item xs={11} sm={5} md={3}>
-              <YourPartner />
-            </Grid>
-            <Grid item align="center" xs={12} sm={5} md={7}>
-              <div>
-                <br />
-
-                <Paper id="cardDesign">
-                  <Typography align="center" variant="h6" gutterBottom>
-                    Your Hotel:
-                    <br />
-                  </Typography>
-                  <HotelDisplay
-                    width={this.state.settings22.width}
-                    height={this.state.settings22.height}
-                    src={this.state.params22}
-                  />
-                </Paper>
-                <br />
-              </div>
-            </Grid>
-            <Grid item xs={1}>
-              <div />
-            </Grid>
-            <Grid item xs={1}>
-              <div />
-            </Grid>
-            <Grid item position="center" xs={10} sm={10} md={10}>
-              {this.state.hasFlights ? (
+            <Grid container spacing={12}>
+              <Grid item xs={1}>
+                <div />
+              </Grid>
+              <Grid item xs={11} sm={5} md={3}>
+                <YourPartner />
+              </Grid>
+              <Grid item align="center" xs={12} sm={5} md={7}>
                 <div>
+                  <br />
+
                   <Paper id="cardDesign">
                     <Typography align="center" variant="h6" gutterBottom>
-                      Your Flight Options:
-                      <hr />
+                      Your Hotel:
+                    <br />
                     </Typography>
-                    <Flights embark={this.state.userAir.origin} dest={this.state.userAir.destination} flightNo={this.state.userAir.flightno} airline={this.state.userAir.airline}/>
-                    <Flights embark={this.state.partnerAir.origin} dest={this.state.partnerAir.destination} flightNo={this.state.partnerAir.flightno} airline={this.state.partnerAir.airline}/>
+                    <HotelDisplay
+                      width={this.state.settings22.width}
+                      height={this.state.settings22.height}
+                      src={this.state.params22}
+                    />
                   </Paper>
+                  <br />
                 </div>
-              ) : (
+              </Grid>
+              <Grid item xs={1}>
                 <div />
-              )}
-              <br />
+              </Grid>
+              <Grid item xs={1}>
+                <div />
+              </Grid>
+              <Grid item position="center" xs={10} sm={10} md={10}>
+                {this.state.hasFlights ? (
+                  <div>
+                    <Paper id="cardDesign">
+                      <Typography align="center" variant="h6" gutterBottom>
+                        Your Flight Options:
+                      <hr />
+                      </Typography>
+                      <div className="flightCards">
+                        <Grid container spacing={24}>
+                          <Grid item xs={12} sm={6} md={6}>
+                            <Typography align="center" variant="subheading" gutterBottom>
+                              Your Flight Info
+                            </Typography>
+                            <FlightCard embark={this.state.userAir.origin} dest={this.state.userAir.destination} flightNo={this.state.userAir.flightno} airline={this.state.userAir.airline} totalFare={this.state.userTotalFare} />
+                          </Grid>
+                          <Grid item xs={12} sm={6} md={6}>
+                            <Typography align="center" variant="subheading" gutterBottom>
+                              Partner Flight Info
+                            </Typography>
+                            <FlightCard embark={this.state.partnerAir.origin} dest={this.state.partnerAir.destination} flightNo={this.state.partnerAir.flightno} airline={this.state.partnerAir.airline} totalFare={this.state.partnerTotalFare} />
+                          </Grid>
+                        </Grid>
+                      </div>
+                    </Paper>
+                  </div>
+                ) : (
+                    <div />
+                  )}
+                <br />
+              </Grid>
+              <Grid item xs={1} sm={1} md={1} />
+              <Grid item xs={12} sm={6} md={6} />
             </Grid>
-            <Grid item xs={1} sm={1} md={1} />
-            <Grid item xs={12} sm={6} md={6} />
-          </Grid>
-        )}
+          )}
       </div>
     );
   }
